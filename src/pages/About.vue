a6b23ee0c5db49e6f55bf39355432953a062d2ef
YuYuan Lin <sandy6513a@yahoo.com.tw>
2023-09-04 13:27:57 +0800

    delete get email

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,6 +6,5 @@ const user = {}
   <div>
     <h1 class="text-4xl">About</h1>
     <button @click="user.email.send()">Send Email</button>
-    <button @click="user.email.get()">Get Email</button>
   </div>
 </template>
